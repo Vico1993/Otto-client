4399ec34369a828b53ab1437c8380b4612768a38
Victor Piolin <victor.piolin@gmail.com>
2023-12-04 20:31:26 -0500

    feat(articles): Add the possibility to create article and list all articles from a feed

--- otto/feed.go
+++ otto/feed.go
@@ -16,6 +16,10 @@ type FeedListResponse struct {
 	Feeds []Feed `json:"feeds"`
 }
 
+type FeedListArticlesResponse struct {
+	Articles []Article `json:"articles"`
+}
+
 type FeedCreateResponse struct {
 	Feed Feed `json:"feed"`
 }
@@ -162,4 +166,29 @@ func (s *FeedService) ListAll(active bool) []Feed {
 	_ = json.Unmarshal(body, &res)
 
 	return res.Feeds
+}
+
+// Retrieve Feed articles
+func (s *FeedService) ListArticles(feedId string) []Article {
+	req, err := http.NewRequest(
+		http.MethodGet,
+		s.client.BaseURL+"/feeds/"+feedId+"/articles",
+		strings.NewReader(
+			string([]byte{}),
+		),
+	)
+	if err != nil {
+		fmt.Println("Error creating the request to list articles: " + err.Error())
+		return nil
+	}
+
+	body, err := s.client.Do(req)
+	if err != nil {
+		return nil
+	}
+
+	var res FeedListArticlesResponse
+	_ = json.Unmarshal(body, &res)
+
+	return res.Articles
 }
